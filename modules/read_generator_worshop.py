2c3c4a3faa6934695a0153b2fdce556f6a43fcb7
Dorian J. Feistel <dfeistel3@gatech.edu>
2024-01-17 17:55:18 -0500

    updated script

--- modules/read_generator_worshop.py
+++ modules/read_generator_worshop.py
@@ -6,38 +6,6 @@ from fasta_funcitons_class_workshop import load_fasta_workflow_test
 from amplicons_functions_class_worshop import generate_amplicon_workflow_test
 from proportion_functions_workshop import GenomeProporitons
 
-def main_2():
-    # Parameters
-    sequence_length = 5000000
-    fragement_size = 200
-    read_size = 150
-    total_depth_coverage = 100
-    random_seed = None
-
-    fasta_dict = load_fasta_into_dict(sys.argv[1])
-    cov_per_genome = genome_proporitons(fasta_dict, total_depth_coverage)
-
-    for amplicon, full_sequence in fasta_dict.items():
-        genome_name = amplicon.split("~~~")[1]
-        if genome_name in cov_per_genome:
-            genome_cov = cov_per_genome[genome_name]
-        seq_len = len(full_sequence)
-        ## list of indicies based on parameters above
-        fragemnt_index_list = fragment_indices_list(
-            seq_len=seq_len,
-            frag_len=fragement_size,
-            read_len=read_size,
-            depth=genome_cov,
-            seed=random_seed
-        )
-
-
-        for index in fragemnt_index_list:
-            start, end = index
-            fragemnt = full_sequence[start:end]
-            f, r = fragemnt[:read_size], fragemnt[-read_size-1:-1]
-            print(f"name: {amplicon}\nforward: {f}\nreverse: {r}\n")
-
 
 class ReadGenerator():
     def __init__(
@@ -51,10 +19,9 @@ class ReadGenerator():
     def add_proportion_column(self):
         self.df_amplicon['proportion'] = self.df_amplicon['reference_defline'].map(self.proportion_dict)
     
-    def fragment_indices_list_df(
+    def read_indices_og(
                 self,
-                frag_length: int=250,
-                read_length: int=150,
+                read_length: int=250,
                 coverage_depth: int=60,
                 seed=None
         ) -> list:
@@ -74,50 +41,92 @@ class ReadGenerator():
             # Iterate over DataFrame rows
             for idx, row in self.df_amplicon.iterrows():
                 total_reads = int(row['total_reads'])
-                upper_limit = int(row['length_bp'] + 1 - frag_length)
+                upper_limit = int(row['length_bp'] + 1 - read_length)
 
                 # Generate indices
                 forward_indices = np.sort(np.random.randint(0, upper_limit, size=total_reads))
-                reverse_indices = forward_indices + frag_length
+                reverse_indices = forward_indices + read_length
                 read_indicies = list(zip(forward_indices, reverse_indices))
                 
                 # Store indices in DataFrame
                 self.df_amplicon.at[idx, 'read_indicies'] = read_indicies
+            self.df_amplicon.to_csv("temp.tsv", sep='\t', index=False)
             print(self.df_amplicon)
+
+    def create_reads_from_amplicons(
+                    self,
+                    read_length: int=250,
+                    coverage_depth: int=60
+                    ):
+                '''
+                Create reads from amplicons
+                '''
+
+                # calulate total reads need per amplicon for desired sequencing depth
+                self.df_amplicon['total_reads'] = coverage_depth * self.df_amplicon['length_bp'] // read_length
+                
+                self.df_amplicon['R1_read'] = None
+                self.df_amplicon['R2_read'] = None
+
+                # Iterate over DataFrame rows to get reads
+                for idx, row in self.df_amplicon.iterrows():
+
+                    R1_read = row['amplicon_sequence'][:read_length]
+                    R2_index_start = row['length_bp'] - read_length
+                    R2_read = row['amplicon_sequence'][R2_index_start:]
+                    self.df_amplicon.at[idx, 'R1_read'] = R1_read
+                    self.df_amplicon.at[idx, 'R2_read'] = R2_read
+                
+    def write_reads(self):
+        for idx, row in self.df_amplicon.iterrows():
+            amplicon_defline = ':'.join([
+                row['reference_defline'], 
+                row['primer_scheme'], 
+                row['primer_name']
+            ])
+
+            R1 = row['R1_read']
+            R2 = row['R2_read']
+
+            for read_number in range(1,row['total_reads']+1):
+                #forward reads
+                fastq_line_R1 = ReadGenerator.name_fastq_read(
+                    amplicon_defline=amplicon_defline,
+                    read_sequence=R1,
+                    read_number=read_number
+                    )
+                fastq_line_R2 = ReadGenerator.name_fastq_read(
+                    amplicon_defline=amplicon_defline,
+                    read_sequence=R2,
+                    read_number=read_number,
+                    reverse=True
+                    )
+                ReadGenerator.write_fastq(R1=fastq_line_R1, R2=fastq_line_R2, fastq_name="temp")
+    
     @staticmethod
     def name_fastq_read(
             amplicon_defline: str,
             read_sequence:str, 
-            read_count:int, 
+            read_number:int, 
             reverse: bool=False
             ) -> list:
-        '''
-        add the name R1 or R2 to the defline of the read
-            quality scores are (as of now) ignored and are given a Q40
-
-        :param amplicon_defline: defline used in amplicon for tracking 
-        :param sequence: Forward/R1 or reverse/R2 sequence
-        :param read_count: Integer used in paired end reads
-        :param reverse: set True if read is R2, else False is R1
-        :return:
-        '''
+        
         if reverse:
             read_type = "R2"
         else:
             read_type = "R1"
 
-        defline = f"@{amplicon_defline}:{read_type}_{read_count}"
-        
-        yield [defline, read_sequence, "+", "I"*len(read_sequence)]
+        defline = f"@{amplicon_defline}:{read_type}_{read_number}"
+        #currently there is no quality score implimentation 
+        return [defline, read_sequence, "+", "I"*len(read_sequence)]
     
     @staticmethod
-    def write_fastq(amplicon_reads_list:dict, fastq_name:str):
-        with open(fastq_name, "w") as wf, open(amplicon_reads_list, 'r') as reads:
-            for read in reads:
-                read_out = '\n'.join(read)
-                #read_out = f"{read[0]}\n{read[1]}\n{read[2]}\n{read[3]}"
-                wf.write(read_out)
-
+    def write_fastq(R1, R2, fastq_name:str):
+        with open(fastq_name + "_R1.fastq", "a") as wf_r1, open(fastq_name + "_R2.fastq", "a") as wf_r2:
+            R1_output = f"{R1[0]}\n{R1[1]}\n{R1[2]}\n{R1[3]}\n"
+            R2_output = f"{R2[0]}\n{R2[1]}\n{R2[2]}\n{R2[3]}\n"
+            wf_r1.write(R1_output)
+            wf_r2.write(R2_output)
 
     # def read_slicing()
 
@@ -132,9 +141,12 @@ def read_generator_workflow_test():
     proportion_dict = GenomeProporitons(reference_fasta_dict=reference_fasta_dict).randomProportions()
     
     rg = ReadGenerator(df_amplicon=df_amplicon, proportion_dict=proportion_dict)
+    # combine propriton values on defline columns
     rg.add_proportion_column()
-    rg.fragment_indices_list_df()
-    print(df_amplicon['total_reads'].sum())
+    # get list of indicies for reads
+    rg.create_reads_from_amplicons()
+    # create reads
+    rg.write_reads()
 
 
 if __name__ == "__main__":
